66110fdd9d1b425b7ff25bb7c2f81f098ce343f7
Ferhat Şahin <frhtshn@gmail.com>
2026-01-04 18:33:57 +0200

    deploy fix

--- deploy_maindb.sql
+++ deploy_maindb.sql
@@ -7,38 +7,38 @@ CREATE SCHEMA IF NOT EXISTS routing;
 CREATE SCHEMA IF NOT EXISTS security;
 
 -- CATALOG TABLES
-\i db/tables/catalog/countries.sql
-\i db/tables/catalog/currencies.sql
-\i db/tables/catalog/currency_rates.sql
-\i db/tables/catalog/games.sql
-\i db/tables/catalog/languages.sql
-\i db/tables/catalog/localization_keys.sql
-\i db/tables/catalog/localization_values.sql
-\i db/tables/catalog/operation_types.sql
-\i db/tables/catalog/provider_settings.sql
-\i db/tables/catalog/provider_types.sql
-\i db/tables/catalog/providers.sql
-\i db/tables/catalog/transaction_types.sql
+\i core/tables/catalog/countries.sql
+\i core/tables/catalog/currencies.sql
+\i core/tables/catalog/currency_rates.sql
+\i core/tables/catalog/games.sql
+\i core/tables/catalog/languages.sql
+\i core/tables/catalog/localization_keys.sql
+\i core/tables/catalog/localization_values.sql
+\i core/tables/catalog/operation_types.sql
+\i core/tables/catalog/provider_settings.sql
+\i core/tables/catalog/provider_types.sql
+\i core/tables/catalog/providers.sql
+\i core/tables/catalog/transaction_types.sql
 
 -- CORE TABLES
-\i db/tables/core/companies.sql
-\i db/tables/core/tenants.sql
-\i db/tables/core/tenant_currencies.sql
-\i db/tables/core/tenant_games.sql
-\i db/tables/core/tenant_languages.sql
-\i db/tables/core/tenant_providers.sql
-\i db/tables/core/tenant_settings.sql
+\i core/tables/core/companies.sql
+\i core/tables/core/tenants.sql
+\i core/tables/core/tenant_currencies.sql
+\i core/tables/core/tenant_games.sql
+\i core/tables/core/tenant_languages.sql
+\i core/tables/core/tenant_providers.sql
+\i core/tables/core/tenant_settings.sql
 
 -- ROUTING TABLES
-\i db/tables/routing/callback_routes.sql
-\i db/tables/routing/provider_callbacks.sql
-\i db/tables/routing/provider_endpoints.sql
+\i core/tables/routing/callback_routes.sql
+\i core/tables/routing/provider_callbacks.sql
+\i core/tables/routing/provider_endpoints.sql
 
 -- SECURITY TABLES
-\i db/tables/security/provider_secrets.sql
-\i db/tables/security/tenant_secrets.sql
+\i core/tables/security/provider_secrets.sql
+\i core/tables/security/tenant_secrets.sql
 
 -- FUNCTIONS
--- \i db/functions/your_function.sql
+-- \i core/functions/your_function.sql
 
 COMMIT;
--- deploy_tenantdb.sql
+++ deploy_tenantdb.sql
@@ -7,26 +7,26 @@ CREATE SCHEMA IF NOT EXISTS transaction;
 CREATE SCHEMA IF NOT EXISTS wallet;
 
 -- AUTH TABLES
-\i tenantdb/tables/auth/players.sql
-\i tenantdb/tables/auth/player_categories.sql
-\i tenantdb/tables/auth/player_classification.sql
-\i tenantdb/tables/auth/player_credentials.sql
-\i tenantdb/tables/auth/player_groups.sql
+\i tenant_/tables/auth/players.sql
+\i tenant_/tables/auth/player_categories.sql
+\i tenant_/tables/auth/player_classification.sql
+\i tenant_/tables/auth/player_credentials.sql
+\i tenant_/tables/auth/player_groups.sql
 
 -- PROFILE TABLES
-\i tenantdb/tables/profile/player_identity.sql
-\i tenantdb/tables/profile/player_profile.sql
+\i tenant_/tables/profile/player_identity.sql
+\i tenant_/tables/profile/player_profile.sql
 
 -- TRANSACTION TABLES
-\i tenantdb/tables/transaction/transactions.sql
-\i tenantdb/tables/transaction/transaction_workflows.sql
-\i tenantdb/tables/transaction/transaction_workflow_actions.sql
+\i tenant_/tables/transaction/transactions.sql
+\i tenant_/tables/transaction/transaction_workflows.sql
+\i tenant_/tables/transaction/transaction_workflow_actions.sql
 
 -- WALLET TABLES
-\i tenantdb/tables/wallet/wallets.sql
-\i tenantdb/tables/wallet/wallet_snapshots.sql
+\i tenant_/tables/wallet/wallets.sql
+\i tenant_/tables/wallet/wallet_snapshots.sql
 
 -- FUNCTIONS
--- \i tenantdb/functions/your_function.sql
+-- \i tenant_/functions/your_function.sql
 
 COMMIT;
