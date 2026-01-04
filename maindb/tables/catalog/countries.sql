99b4b767f74e3bc79a8726ebaed3bc0225593d8f
Ferhat Şahin <frhtshn@gmail.com>
2026-01-04 14:14:59 +0200

    data

--- maindb/tables/catalog/countries.sql
+++ maindb/tables/catalog/countries.sql
@@ -2,8 +2,6 @@ DROP TABLE IF EXISTS catalog.countries CASCADE;
 
 CREATE TABLE catalog.countries (
     country_code character(2) PRIMARY KEY,
-    country_name varchar(100) NOT NULL,
-    is_active boolean NOT NULL DEFAULT true,
-    created_at timestamp without time zone NOT NULL DEFAULT now(),
-    updated_at timestamp without time zone NOT NULL DEFAULT now()
+    country_code_a3 character(3) NOT NULL UNIQUE,
+    country_name varchar(100) NOT NULL
 );
