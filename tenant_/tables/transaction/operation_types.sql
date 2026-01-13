e1865fef4952658a828fcc7145f7b9729c630845
Ferhat Şahin <frhtshn@gmail.com>
2026-01-13 09:49:10 +0300

    Update operation_types.sql

--- tenant_/tables/transaction/operation_types.sql
+++ tenant_/tables/transaction/operation_types.sql
@@ -1,1 +1,9 @@
+DROP TABLE IF EXISTS finance.operation_types CASCADE;
 
+CREATE TABLE finance.operation_types (
+    id                smallserial PRIMARY KEY,
+    code              varchar(30) NOT NULL UNIQUE,
+    wallet_effect     smallint NOT NULL,
+    affects_balance   boolean NOT NULL,
+    affects_locked    boolean NOT NULL
+);
