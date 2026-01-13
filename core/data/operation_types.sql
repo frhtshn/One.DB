3951f181cb836c7723c10528f71d11d215161ec2
Ferhat Şahin <frhtshn@gmail.com>
2026-01-13 09:39:55 +0300

    Update operation_types.sql

--- core/data/operation_types.sql
+++ core/data/operation_types.sql
@@ -1,1 +1,8 @@
-
+INSERT INTO catalog.operation_types
+(code, wallet_effect, affects_balance, affects_locked, description)
+VALUES
+('DEBIT',   -1, true,  false, 'Balance decrease'),
+('CREDIT',   1, true,  false, 'Balance increase'),
+('HOLD',     0, false, true,  'Lock funds'),
+('RELEASE',  0, false, true,  'Release locked funds'),
+('NOOP',     0, false, false, 'No wallet effect');
