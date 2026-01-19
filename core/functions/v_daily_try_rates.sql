e3e4b1d6d740a648321fb09c8c01c873b4960b59
Ferhat Şahin <frhtshn@gmail.com>
2026-01-19 13:16:51 +0300

    Delete core/functions/v_daily_try_rates.sql

--- core/functions/v_daily_try_rates.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW catalog.v_daily_try_rates AS
-SELECT DISTINCT ON (rate_date, quote_currency)
-       rate_date,
-       quote_currency,
-       rate
-FROM catalog.currency_rates
-WHERE base_currency = 'TRY'
-ORDER BY rate_date, quote_currency, created_at DESC;
-
