bcf55ecbb4a33f72fc395c1a5242e9793e9af2b2
Ferhat Şahin <frhtshn@gmail.com>
2026-01-19 17:10:02 +0300

    Create currency_rates_latest table with schema

--- tenant/tables/finance/currency_rates_latest.sql
+++ tenant/tables/finance/currency_rates_latest.sql
@@ -1,1 +1,12 @@
+DROP TABLE IF EXISTS finance.currency_rates_latest CASCADE;
 
+CREATE TABLE finance.currency_rates_latest (
+    provider VARCHAR(30) NOT NULL,
+    provider_base_currency CHAR(3) NOT NULL,
+    target_currency CHAR(3) NOT NULL,
+
+    rate NUMERIC(18,8) NOT NULL,
+    rate_timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+
+    PRIMARY KEY (provider, provider_base_currency, target_currency)
+);
