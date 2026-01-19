af2d4532f19c7947dcc316d1a3c2fb8c8bfa2a95
Ferhat Şahin <frhtshn@gmail.com>
2026-01-19 13:15:06 +0300

    Create view v_cross_rates for currency conversion

--- tenant_/functions/v_cross_rates.sql
+++ tenant_/functions/v_cross_rates.sql
@@ -1,1 +1,10 @@
+CREATE VIEW finance.v_cross_rates AS
+SELECT
+    r1.rate_date,
+    r1.quote_currency AS from_currency,
+    r2.quote_currency AS to_currency,
+    r2.rate / r1.rate AS rate
+FROM finance.v_daily_base_rates r1
+JOIN finance.v_daily_base_rates r2
+  ON r1.rate_date = r2.rate_date;
 
