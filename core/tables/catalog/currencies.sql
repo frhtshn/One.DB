c11b2f98b52c75a672f7176175f78d44c8d42f16
Ferhat Şahin <frhtshn@gmail.com>
2026-01-19 11:54:18 +0300

    Update currencies.sql

--- core/tables/catalog/currencies.sql
+++ core/tables/catalog/currencies.sql
@@ -1,8 +1,11 @@
 DROP TABLE IF EXISTS catalog.currencies CASCADE;
 
 CREATE TABLE catalog.currencies (
-    currency_code character(3) PRIMARY KEY,
-    currency_name varchar(100) NOT NULL,
-    symbol varchar(10),
-    is_active boolean NOT NULL DEFAULT true
+    currency_code CHAR(3) PRIMARY KEY,     -- ISO 4217
+    currency_name VARCHAR(100) NOT NULL,
+    symbol VARCHAR(10),
+    numeric_code SMALLINT,                  
+    is_active BOOLEAN NOT NULL DEFAULT true,
+    created_at TIMESTAMP NOT NULL DEFAULT now()
 );
+
