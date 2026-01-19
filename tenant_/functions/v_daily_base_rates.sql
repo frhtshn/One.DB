b5db42f8697a5663f2eecfe5586965b558c308c5
Ferhat Şahin <frhtshn@gmail.com>
2026-01-19 13:15:22 +0300

    Create view v_daily_base_rates for currency rates

--- tenant_/functions/v_daily_base_rates.sql
+++ tenant_/functions/v_daily_base_rates.sql
@@ -1,1 +1,8 @@
+CREATE VIEW finance.v_daily_base_rates AS
+SELECT DISTINCT ON (rate_date, quote_currency)
+       rate_date,
+       quote_currency,
+       rate
+FROM finance.currency_rates
+ORDER BY rate_date, quote_currency, fetched_at DESC;
 
