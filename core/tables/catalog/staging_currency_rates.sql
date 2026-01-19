94a4d58af8bd31e577eb2ced0c9e0e7714ddb90e
Ferhat Şahin <frhtshn@gmail.com>
2026-01-19 13:25:58 +0300

    Delete core/tables/catalog/staging_currency_rates.sql

--- core/tables/catalog/staging_currency_rates.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE catalog.staging_currency_rates (
-    base_currency CHAR(3),
-    quote_currency CHAR(3),
-    rate NUMERIC(18,8),
-    rate_date DATE,
-    source VARCHAR(50),
-    created_at TIMESTAMP
-);
-
