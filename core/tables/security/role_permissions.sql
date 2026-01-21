0607105992a0458c6aef7271a35197bd16f5bf88
Ferhat Şahin <frhtshn@gmail.com>
2026-01-21 13:24:01 +0300

    Modify role_permissions table structure and references

--- core/tables/security/role_permissions.sql
+++ core/tables/security/role_permissions.sql
@@ -1,7 +1,10 @@
 DROP TABLE IF EXISTS security.role_permissions CASCADE;
 
 CREATE TABLE security.role_permissions (
-    role_id BIGINT NOT NULL REFERENCES security.roles(id),
-    permission_id BIGINT NOT NULL REFERENCES security.permissions(id),
-    PRIMARY KEY (role_id, permission_id)
+    role_id BIGINT NOT NULL
+        REFERENCES security.tenant_roles(id) ON DELETE CASCADE,
+    permission_code VARCHAR(100) NOT NULL
+        REFERENCES security.permissions(code),
+    PRIMARY KEY (role_id, permission_code)
 );
+
