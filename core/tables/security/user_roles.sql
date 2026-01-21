377337b991270ad1554c628974ec12c8361ef854
Ferhat Şahin <frhtshn@gmail.com>
2026-01-21 13:24:44 +0300

    Rename user_roles table to tenant_user_roles

--- core/tables/security/user_roles.sql
+++ core/tables/security/user_roles.sql
@@ -1,13 +1,10 @@
 DROP TABLE IF EXISTS security.user_roles CASCADE;
 
-CREATE TABLE security.user_roles (
+CREATE TABLE security.tenant_user_roles (
     user_id BIGINT NOT NULL,
-        --REFERENCES security.users(id)
-        --ON DELETE CASCADE,
-
-    role_id BIGINT NOT NULL,
-        --REFERENCES security.roles(id)
-        --ON DELETE CASCADE,
-
-    PRIMARY KEY (user_id, role_id)
+    tenant_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL
+        REFERENCES security.roles(id),
+    PRIMARY KEY (user_id, tenant_id, role_id)
 );
+
