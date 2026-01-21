7eefea3aa01384922e1f92bf54ad8463a284f9d7
Ferhat Şahin <frhtshn@gmail.com>
2026-01-21 13:27:07 +0300

    Change foreign key reference from roles to tenant_roles

--- core/tables/security/user_roles.sql
+++ core/tables/security/user_roles.sql
@@ -4,7 +4,7 @@ CREATE TABLE security.tenant_user_roles (
     user_id BIGINT NOT NULL,
     tenant_id BIGINT NOT NULL,
     role_id BIGINT NOT NULL
-        REFERENCES security.roles(id),
+        REFERENCES security.tenant_roles(id),
     PRIMARY KEY (user_id, tenant_id, role_id)
 );
 
