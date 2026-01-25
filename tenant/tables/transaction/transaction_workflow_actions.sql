36de8a3f7d5246ee00e15f62796c5e518603e124
Ferhat Şahin <frhtshn@gmail.com>
2026-01-25 23:47:53 +0200

    feat: Add `transaction_workflows` and `transaction_workflow_actions` tables for managing transaction processes.

--- tenant/tables/transaction/transaction_workflow_actions.sql
+++ tenant/tables/transaction/transaction_workflow_actions.sql
@@ -4,7 +4,8 @@ CREATE TABLE transaction.transaction_workflow_actions (
     id bigserial PRIMARY KEY,
     workflow_id bigint NOT NULL,
     action varchar(30) NOT NULL,
-    performed_by varchar(50),
+    performed_by_id bigint,                 -- İşlemi yapan ID
+    performed_by_type varchar(30) NOT NULL, -- BO_USER / SYSTEM / PLAYER
     note varchar(255),
     created_at timestamp without time zone NOT NULL DEFAULT now()
 );
--- tenant/tables/transaction/transaction_workflows.sql
+++ tenant/tables/transaction/transaction_workflows.sql
@@ -6,6 +6,8 @@ CREATE TABLE transaction.transaction_workflows (
     workflow_type varchar(30) NOT NULL,
     status varchar(30) NOT NULL,
     reason varchar(255),
+    created_by_id bigint,                   -- Workflow'u başlatan kullanıcı (BO User or System)
+    assigned_to_id bigint,                  -- Atanan kullanıcı (Opsiyonel)
     created_at timestamptz NOT NULL DEFAULT now(),
     updated_at timestamptz NOT NULL DEFAULT now()
 );
